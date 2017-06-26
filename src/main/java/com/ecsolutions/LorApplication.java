6a9fc04d8b287072e0895fa3252cf7a7a7bca37c
BAODANZHU <maxeric@126.com>
2017-06-26 17:50:12 +0800

    Initial commit

---
+++ src/main/java/com/ecsolutions/LorApplication.java
@@ -0,0 +1,12 @@
+package com.ecsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LorApplication.class, args);
+	}
+}
