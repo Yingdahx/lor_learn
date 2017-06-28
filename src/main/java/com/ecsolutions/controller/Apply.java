7eb876176b6eb2dff81a13a5d8634114a0892dbb
BAODANZHU <maxeric@126.com>
2017-06-28 15:52:50 +0800

    表单提交示例

--- src/main/java/com/ecsolutions/controller/Apply.java
+++ src/main/java/com/ecsolutions/controller/Apply.java
@@ -1,7 +1,11 @@
 package com.ecsolutions.controller;
 
+import com.ecsolutions.entity.Apply_entity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 /**
  * Created by Administrator on 2017/6/27.
@@ -13,7 +17,16 @@ public class Apply {
 }
 
     @GetMapping("/apply")
-    public String getApplyPage() {
+    public String getApplyPage(Model model) {
+        model.addAttribute("apply_entity", new Apply_entity());
+        return "forms";
+    }
+
+    @PostMapping("/app")
+    public String saveApplyInfo(@ModelAttribute Apply_entity apply_entity, Model model) {
+        model.addAttribute("apply_entity", apply_entity);
+        System.out.println(apply_entity.getCustomerManager());
+        System.out.println(apply_entity.getRegisterType());
         return "forms";
     }
 }
---
+++ src/main/java/com/ecsolutions/entity/Apply_entity.java
@@ -0,0 +1,25 @@
+package com.ecsolutions.entity;
+
+/**
+ * Created by Administrator on 2017/6/28.
+ */
+public class Apply_entity {
+    private String customerManager;
+    private String registerType;
+
+    public String getCustomerManager() {
+        return customerManager;
+    }
+
+    public void setCustomerManager(String customerManager) {
+        this.customerManager = customerManager;
+    }
+
+    public String getRegisterType() {
+        return registerType;
+    }
+
+    public void setRegisterType(String registerType) {
+        this.registerType = registerType;
+    }
+}
