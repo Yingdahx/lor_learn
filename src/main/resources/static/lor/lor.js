9e40a7b0cc0b176dd1b14aeda0cdc31d66f0cc4c
zhang_jianli <zhaoji8324@163.com>
2017-06-30 13:37:13 +0800

    zjl commit

---
+++ src/main/resources/static/lor/lor.js
@@ -0,0 +1,18 @@
+/**
+ * Created by tim on 2017/6/28.
+ */
+
+function getProjectName()
+{
+    var lcoUrl =$("link").attr("href");
+    var index=lcoUrl.indexOf("/",1);
+    //alert(index);
+  var projectName=lcoUrl.substr(0,index);
+    //alert(projectName);
+    return projectName;
+}
+
+function fnGetSelected( oTableLocal )
+{
+    return oTableLocal.$('tr.row_selected');
+}
