6be4c9179f4e46396c6782008e07c6176efcc8a6
JohnHe2015 <xiaozhemu@163.com>
2017-07-04 16:59:09 +0800

    个人页面

---
+++ src/main/java/com/ecsolutions/dao/Customer_DAO.java
@@ -0,0 +1,65 @@
+package com.ecsolutions.dao;
+
+import org.apache.ibatis.annotations.ResultType;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Administrator on 2017/6/28.
+ */
+
+@Repository
+public interface Customer_DAO {
+    // select idType
+    @Select("SELECT distinct pidTypeId,name FROM adsuser.PIDTypeText WHERE localeid=1")
+    @ResultType(HashMap.class)
+    List<HashMap<String,String>> getIdType();
+
+    // select 婚姻状况
+    @Select("SELECT distinct maritalStatusId,name FROM adsuser.maritalStatusText  WHERE localeid=1")
+    @ResultType(HashMap.class)
+    List<HashMap<String,String>> getMaritalStatus();
+
+    //select 居住状况
+    @Select("select distinct typeid,name from dbo.LivingConditionsText where localeid=1")
+    @ResultType(HashMap.class)
+    List<HashMap<String,String>> getJuzhuStatus();
+
+    //select 职业
+    @Select("select distinct typeid,name from dbo.VocationalText where localeid=1")
+    @ResultType(HashMap.class)
+    List<HashMap<String,String>> getZhiye();
+
+    //select 职务
+    @Select("select distinct typeid,name from dbo.DutyText where localeid=1")
+    @ResultType(HashMap.class)
+    List<HashMap<String,String>> getZhiWu();
+
+    //select 职称
+    @Select("select distinct typeid,name from dbo.DutyTitleText where localeid=1")
+    @ResultType(HashMap.class)
+    List<HashMap<String,String>> getZhiChen();
+
+    //select 年收入币种
+    @Select("select distinct ccyid,ccytext from dbo.ccyText where localeid=4")
+    @ResultType(HashMap.class)
+    List<HashMap<String,String>> getCcy();
+
+    //select 最高学历
+    @Select("select distinct educationLevelId,name from adsuser.educationLevelText where localeid=1 order by educationLevelId desc")
+    @ResultType(HashMap.class)
+    List<HashMap<String,String>> getXueli();
+
+    //select 最高学位
+    @Select("select distinct TypeId,name from dbo.highestDegreeText where localeid=1")
+    @ResultType(HashMap.class)
+    List<HashMap<String,String>> getXueWei();
+//    @Select("SELECT DISTINCT  FROM  )")
+//    @ResultType(String.class)
+//    List<String> getFinancingList();
+
+
+}
---
+++ src/main/java/com/ecsolutions/service/Customer_Service.java
@@ -0,0 +1,11 @@
+package com.ecsolutions.service;
+
+import com.ecsolutions.entity.Customer_entity;
+
+/**
+ * Created by Administrator on 2017/6/28.
+ */
+public interface Customer_Service {
+    Customer_entity getPopupInfo();
+
+}
